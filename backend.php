e834173f38817937e17803a515948d30da1426ca
Abdur Rehman <abdurrehmansiddiqui143@gmail.com>
2022-11-03 13:34:50 +0500

    second commit

--- backend.php
+++ backend.php
@@ -1,11 +1,21 @@
 <?php
 
+    echo "hello World";
     $n= $_POST['my_name'];
     $s=$_POST['my_sku'];
     $d=$_POST['my_description'];
 
     $conn=new mysqli('localhost','root','','form');
-    
+    if(!$conn)
+    {
+        die("connection was not made succesfully");
+    }
+    else{
+
+        $php="insert into form(My_name,My_sku,Description)values($n,$s,$d)";
+
+
+    }
 
 
 
