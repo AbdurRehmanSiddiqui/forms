9860d94ff0e6f62bef8e8a1601b2ce1704f27fda
Abdur Rehman <abdurrehmansiddiqui143@gmail.com>
2022-11-03 17:04:45 +0500

    updated

--- backend.php
+++ backend.php
@@ -1,22 +1,24 @@
 <?php
 
-    echo "hello World";
-    $n= $_POST['my_name'];
-    $s=$_POST['my_sku'];
-    $d=$_POST['my_description'];
+echo "hello World";
+$n = $_POST['my_name'];
+$s = $_POST['my_sku'];
+$d = $_POST['my_description'];
 
-    $conn=new mysqli('localhost','root','','form');
-    if(!$conn)
-    {
-        die("connection was not made succesfully");
-    }
-    else{
-
-        $php="insert into form(My_name,My_sku,Description)values($n,$s,$d)";
+//echo $n;
+//echo $s;
 
 
-    }
+$conn = new mysqli('localhost', 'root', 'root', 'forms');
+if (!$conn) {
+    die("connection was not made succesfully");
+} else {
+    echo "connection successful";
 
 
 
+
+    $php ="insert into froms_data(product_name,sku,description)values('$n', '$s', '$d')";
+    mysqli_query($conn, $php);
+}
 ?>
