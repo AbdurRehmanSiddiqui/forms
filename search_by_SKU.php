1c9949624c67966496db30728a1e748d0f779ac3
Abdur Rehman <abdurrehmansiddiqui143@gmail.com>
2022-11-14 15:00:14 +0500

    Search by SKU  UPDATED

--- search_by_SKU.php
+++ search_by_SKU.php
@@ -11,15 +11,14 @@ if (!$conn) {
     $result = mysqli_query($conn, $query);
 
     while ($data = mysqli_fetch_assoc($result)) {
-
-            $html = '<tr>';
-            $html = $html . '<td>' . $data['ID'] . '</td>';
-            $html = $html . '<td>' . $data['product_name'] . '</td>';
-            $html = $html . '<td>' . $data['sku'] . '</td>';
-            $html = $html . '<td>' . $data['description'] . '</td>';
-            $html = $html . '</tr>';
-            echo $html;
-
+        $html = '<tr>';
+        $html = $html . '<td>' . $data['ID'] . '</td>';
+        $html = $html . '<td>' . $data['product_name'] . '</td>';
+        $html = $html . '<td>' . $data['sku'] . '</td>';
+        $html = $html . '<td>' . $data['description'] . '</td>';
+        $html = $html . '<td>'.'<a href="edit.php?pk='.$data['ID'].'">Edit</a></td>';
+        $html = $html . '</tr>';
+        echo $html;
     }
 }
 
